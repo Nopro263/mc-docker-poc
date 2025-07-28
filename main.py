770137d394a9fd7742287c6a356c157695d6dbea
Noah <82201000+Nopro263@users.noreply.github.com>
2025-07-28 20:26:07 +0200

    added start and stop

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.responses import RedirectResponse
 from typing import Iterable
 import starlette.websockets
 
-from manager import Manager
+from manager import Manager, ServerData
 
 app = FastAPI()
 manager = Manager()
@@ -23,14 +23,30 @@ async def console_endpoint(websocket: WebSocket, id: str):
             manager.send_console_in(id, data)
         except starlette.websockets.WebSocketDisconnect:
             return
+        except IOError:
+            await websocket.send_json({
+                    "console_out": "SERVER NOT STARTED"
+                })
 
 @app.get("/api/servers")
-def get_severs() -> Iterable[str]:
-    return manager.servers.keys()
+def get_severs() -> Iterable[ServerData]:
+    return [x.get_data() for x in manager.servers.values()]
+
+@app.get("/api/server/{id:str}")
+def get_sever(id:str) -> ServerData:
+    return manager.servers[id].get_data()
+
+@app.post("/api/server/{id:str}/start")
+def start_sever(id:str) -> None:
+    return manager.servers[id].start()
+
+@app.post("/api/server/{id:str}/stop")
+def stop_sever(id:str) -> None:
+    return manager.servers[id].stop()
 
 @app.post("/api/servers")
-def create_server() -> str:
-    return manager.create_server()
+def create_server() -> ServerData:
+    return manager.servers[manager.create_server()].get_data()
 
 @app.get("/")
 def index():
--- manager.py
+++ manager.py
@@ -5,6 +5,8 @@ from typing import Dict, List
 import selectors
 from threading import Thread
 import asyncio
+from pydantic import BaseModel
+from socket import SocketIO
 
 class Poller(Thread):
     def run(self) -> None:
@@ -39,7 +41,7 @@ class Manager:
         server.send_to_console_in(data + "\n")
     
     def find_running_servers(self):
-        containers = self.client.containers.list(filters={
+        containers = self.client.containers.list(all=True,filters={
             "label": "mc-docker"
         })
 
@@ -71,19 +73,50 @@ class Manager:
 
         return id
 
+class ServerData(BaseModel):
+    id: str
+    online: bool
+    name: str
+
 class Server:
     def __init__(self, id: str, container: Container) -> None:
         self.id = id
         self.container = container
         self.console_listeners: List[WebSocket] = []
 
+        self.attach_socket: SocketIO = None # type: ignore
+
+        if self.container.status == "running":
+            self._connect()
+
+    
+    def _connect(self):
         self.attach_socket = self.container.attach_socket(params={'stdin': 1, 'stdout': 1, 'stderr': 1, 'stream':1})
-        self.attach_socket._sock.setblocking(False)
-        self.attach_socket._writing = True
+        self.attach_socket._sock.setblocking(False) # type: ignore
+        self.attach_socket._writing = True # type: ignore
 
         poller.register(self.attach_socket, self)
     
+    def get_data(self) -> ServerData:
+        self.container.reload()
+        return ServerData(
+            id=self.id,
+            online=self.container.status == "running",
+            name=self.container.name # type: ignore
+        )
+    
+    def start(self):
+        self.container.start()
+        self._connect()
+    
+    def stop(self):
+        self.container.stop(timeout=2)
+        poller.selector.unregister(self.attach_socket)
+        self.attach_socket = None # type: ignore
+    
     def send_to_console_in(self, text: str):
+        if not self.attach_socket:
+            raise IOError("nope")
         self.attach_socket.write(text.encode())
 
     async def send_from_console(self, data: bytes):
@@ -104,4 +137,3 @@ class Server:
                 pass
             finally:
                 self.console_listeners.remove(ws)
-
